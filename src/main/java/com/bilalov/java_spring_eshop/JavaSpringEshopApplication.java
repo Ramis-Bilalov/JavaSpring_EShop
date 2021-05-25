bf533615bc4cc12688e266aa5c5377d6dbd2f4a7
Ramis Bialov <ramiska81@gmail.com>
2021-05-25 13:51:56 +0300

    Initial commit

---
+++ src/main/java/com/bilalov/java_spring_eshop/JavaSpringEshopApplication.java
@@ -0,0 +1,13 @@
+package com.bilalov.java_spring_eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringEshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bilalov/java_spring_eshop/JavaSpringEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilalov.java_spring_eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
